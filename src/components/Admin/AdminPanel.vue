e032cc5b289c5d36272da8b49e6bb13137e91a5c
Pazazamadaz <52623509+Pazazamadaz@users.noreply.github.com>
2024-10-22 20:29:18 +0100

    fixed modal pop up issue.

--- src/components/Admin/AdminPanel.vue
+++ src/components/Admin/AdminPanel.vue
@@ -22,12 +22,18 @@
       </tbody>
     </table>
     <p v-else>No users available to display.</p>
+
+    <ShowLoadingModal />
+    <ShowErrorModal />
+    
   </div>
 </template>
 
 <script setup>
 import useAdminStore from '@/stores/useAdminStore';
 import { onMounted } from 'vue';
+import ShowErrorModal from '@/components/Helpers/ShowErrorModal.vue';
+import ShowLoadingModal from "@/components/Helpers/ShowLoadingModal.vue";
 
 const { fetchUsers, users } = useAdminStore();
 
